d52e494899d73ed678aaf8aea6f4e21478654ee4
Tor Robinson <torrobinson@gmail.com>
2018-04-24 07:33:18 -0600

    Added word square

---
+++ word_square/word_squares_ungolfed.ps1
@@ -0,0 +1,22 @@
+﻿$w="fart"
+
+# word length
+$length = $w|% Le*;
+$lines=@()
+0..($length-1)|%{
+    $letterIndex = $_
+    $line=''
+
+    # Build the top-left corner of the square
+    0..($length-1)|%{
+        $line+=$word[(($letterIndex,$_)[$_-le$letterIndex])]
+    }    
+
+    # Add it to a collection while mirroring it across
+    $lines+=$line+($line[-2..-$line.length]-join'')
+
+}
+
+$linecount = $lines.length
+# Print the lines forward and then backward, mirroring it vertically
+0..($linecount-1)+($linecount-2)..0|%{$lines[$_]}
