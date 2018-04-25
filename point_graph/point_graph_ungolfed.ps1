d95052691a83cd196a76f545c07d5264854c3f97
Tor Robinson <torrobinson@gmail.com>
2018-04-25 09:23:25 -0600

    Final

--- point_graph/point_graph_ungolfed.ps1
+++ point_graph/point_graph_ungolfed.ps1
@@ -1,6 +1,6 @@
 cls
 #param($array)
-$array=2,4,6,8,10,9,8,7,6,5,4,3,2,1,3,5,7,9,11
+$array=1,2,3,4
 $arrayLength=$array.count
 $lines=@();
 $index=0
@@ -12,39 +12,34 @@ $pad={
 
 # count down from the highest value to create the vertical axis
 ($array|sort)[-1]..1|%{
-    $lines+=,
-    [char[]](
-    # The padded value
-    (&$pad $_)+
 
-    # and the vertical axis
-    "|"+
+    # Rather than just printing, store as an array of char arrays so we can access them easier later
 
-    # and the background dots
-    "."*($arrayLength*3-2))
+    $lines+=, # += will APPEND arrays, +=, will add an array TO an array
+        [char[]]( # put together the line's string but cast as a char array
+        # The padded value
+        (&$pad $_)+
 
-    
+        # and the vertical axis
+        "|"+
+
+        # and the background dots
+        "."*($arrayLength*4-2))
 };
-
-
-
-
 
 # Plot the points on our "2D array" of arrays of char arays
 $array|%{
-    $lines[$lines.count-$_][3+$index++*3]='o'
+    $lines[$lines.count-$_][3+$index++*4]='o'
 };
-
-
-
 
 # Print lines
 $lines|%{-join$_};
 
+# Draw the bottom axis:
+#   horizontal line
+"  +"+"-"*($arrayLength*4-2)
 
-# Bottom axis
-#   line
-"  +"+"-"*($arrayLength*3-2)
+#   and bottom numbers, padded
+"  "+(1..$arrayLength|%{&$pad $_ +" "})
 
-#   and numbers
-"  "+1..$arrayLength|%{&$pad $_}
+# When golfing, remove the pad function, it's shorter to just call '{0:00}'-f$variable
