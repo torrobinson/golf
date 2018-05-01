69f275da41ff62e9bdd78a21ed7dd7beb7080849
Tor Robinson <torrobinson@gmail.com>
2018-05-01 11:04:19 -0600

    Added new golf

---
+++ poop_pile/poop_pile.ps1
@@ -0,0 +1,52 @@
+﻿<#
+
+Assuming script is passed a parameter of an integer >=1 that is the number of MIDDLE layers in a pile o poop.
+The topmost layer must have a break in the bottom line which alternates all the way to the bottom
+
+0=   
+ (`\
+(___)
+
+1=
+  (`\
+ (__ )    =1
+(_____)
+
+2 =
+   (`\
+  (__ )
+ ( ____)
+(_______)
+
+3 = 
+    (`\
+   (__ )
+  ( ____)
+ (______ )
+(_________)
+
+#>
+
+
+$middleLayers = 0;
+
+# top
+
+" "*(1+$middleLayers) + "(``\"
+
+## end top
+
+
+# middle
+if($middleLayers-gt0){
+    0..($middleLayers-1)|%{
+    
+        " "*($middleLayers-$_)  + "(" + ("_"," ")[$_%2-ne0] + "_"*(1+$_*2) + ("_"," ")[$_%2-eq0] + ")"
+    }
+}
+# end middle
+
+
+#bottom
+"(" + "_"*(3+$middleLayers*2) + ")"
+#end bottom
